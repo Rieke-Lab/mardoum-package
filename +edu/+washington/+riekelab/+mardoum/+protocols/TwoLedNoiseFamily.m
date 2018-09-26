664653cc06a7e1fec6af159a35442f2e845f54bf
Phil <pmardoum@gmail.com>
2018-09-26 13:06:31 -0700

    Two-LED noise: first pass

--- +edu/+washington/+riekelab/+mardoum/+protocols/TwoLedNoiseFamily.m
+++ +edu/+washington/+riekelab/+mardoum/+protocols/TwoLedNoiseFamily.m
@@ -1,27 +1,20 @@
-classdef LedNoiseFamily < edu.washington.riekelab.protocols.RiekeLabProtocol
-    % Presents families of gaussian noise stimuli to a specified LED and records responses from a specified amplifier.
-    % Each family consists of a set of noise stimuli with the standard deviation of noise starting at startStdv. Each
-    % standard deviation value is repeated repeatsPerStdv times before moving to the next standard deviation value which
-    % is calculated by multiplying startStdv by stdvMultiplier^sdNum. The family is complete when this sequence has been
-    % executed stdvMultiples times.
-    %
-    % For example, with values startStdv = 0.005, stdvMultiplier = 3, stdvMultiples = 3, and repeatsPerStdv = 5, the
-    % sequence of noise stimuli standard deviation values in each family would be: 0.005 five times then 0.015 fives 
-    % times then 0.045 five times.
+classdef TwoLedNoiseFamily < edu.washington.riekelab.protocols.RiekeLabProtocol
     
     properties
-        led                             % Output LED
+        led1                            % Output LED 1
+        led2                            % Output LED 2
         preTime = 100                   % Noise leading duration (ms)
         stimTime = 600                  % Noise duration (ms)
         tailTime = 100                  % Noise trailing duration (ms)
-        frequencyCutoff = 60            % Noise frequency cutoff for smoothing (Hz)
-        numberOfFilters = 4             % Number of filters in cascade for noise smoothing
-        startStdv = 0.005               % First noise standard deviation, post-smoothing (V or norm. [0-1] depending on LED units)
-        stdvMultiplier = 3              % Amount to multiply the starting standard deviation by with each new multiple 
-        stdvMultiples = uint16(3)       % Number of standard deviation multiples in family
-        repeatsPerStdv = uint16(5)      % Number of times to repeat each standard deviation multiple
-        useRandomSeed = false           % Use a random seed for each standard deviation multiple?
-        lightMean = 0.1                 % Noise and LED background mean (V or norm. [0-1] depending on LED units)
+        frequencyCutoff1 = 60           % Noise frequency cutoff for smoothing, LED 1 (Hz)
+        frequencyCutoff2 = 60           % Noise frequency cutoff for smoothing, LED 2 (Hz)
+        numberOfFilters1 = 4            % Number of filters in cascade for noise smoothing, LED 1
+        numberOfFilters2 = 4            % Number of filters in cascade for noise smoothing, LED 2
+        stdv1 = 0.005                   % Noise standard deviation, post-smoothing, LED 1 (V or norm. [0-1] depending on LED units)
+        stdv2 = 0.005                   % Noise standard deviation, post-smoothing, LED 2 (V or norm. [0-1] depending on LED units)
+        mean1 = 0.1                     % Noise and LED background mean, LED 1 (V or norm. [0-1] depending on LED units)
+        mean2 = 0.1                     % Noise and LED background mean, LED 2 (V or norm. [0-1] depending on LED units)
+        useRandomSeed = false           % Use a random seed?
         amp                             % Input amplifier
     end
     
@@ -34,25 +27,19 @@ classdef LedNoiseFamily < edu.washington.riekelab.protocols.RiekeLabProtocol
         interpulseInterval = 0          % Duration between noise stimuli (s)
     end
     
-    properties (Hidden, Dependent)
-        pulsesInFamily
-    end
-    
     properties (Hidden)
-        ledType
+        led1Type
+        led2Type
         ampType
     end
     
     methods
         
-        function n = get.pulsesInFamily(obj)
-            n = obj.stdvMultiples * obj.repeatsPerStdv;
-        end
-        
         function didSetRig(obj)
             didSetRig@edu.washington.riekelab.protocols.RiekeLabProtocol(obj);
             
-            [obj.led, obj.ledType] = obj.createDeviceNamesProperty('LED');
+            [obj.led1, obj.led1Type] = obj.createDeviceNamesProperty('LED');
+            [obj.led2, obj.led2Type] = obj.createDeviceNamesProperty('LED');
             [obj.amp, obj.ampType] = obj.createDeviceNamesProperty('Amp');
         end
         
@@ -64,36 +51,33 @@ classdef LedNoiseFamily < edu.washington.riekelab.protocols.RiekeLabProtocol
             end
         end
         
-        function p = getPreview(obj, panel)
-            p = symphonyui.builtin.previews.StimuliPreview(panel, @()createPreviewStimuli(obj));
-            function s = createPreviewStimuli(obj)
-                s = cell(1, obj.pulsesInFamily);
-                for i = 1:numel(s)
-                    if ~obj.useRandomSeed
-                        seed = 0;
-                    elseif mod(i - 1, obj.repeatsPerStdv) == 0
-                        seed = RandStream.shuffleSeed;
-                    end
-                    s{i} = obj.createLedStimulus(i, seed);
-                end
-            end
-        end
+        % function p = getPreview(obj, panel)
+        %     p = symphonyui.builtin.previews.StimuliPreview(panel, @()createPreviewStimuli(obj));
+        %     function s = createPreviewStimuli(obj)
+        %         s = cell(1, obj.pulsesInFamily);
+        %         for i = 1:numel(s)
+        %             if ~obj.useRandomSeed
+        %                 seed = 0;
+        %             elseif mod(i - 1, obj.repeatsPerStdv) == 0
+        %                 seed = RandStream.shuffleSeed;
+        %             end
+        %             s{i} = obj.createLedStimulus(i, seed);
+        %         end
+        %     end
+        % end
         
         function prepareRun(obj)
             prepareRun@edu.washington.riekelab.protocols.RiekeLabProtocol(obj);
             
             if numel(obj.rig.getDeviceNames('Amp')) < 2
                 obj.showFigure('symphonyui.builtin.figures.ResponseFigure', obj.rig.getDevice(obj.amp));
-                obj.showFigure('symphonyui.builtin.figures.MeanResponseFigure', obj.rig.getDevice(obj.amp), ...
-                    'groupBy', {'stdv'});
+                obj.showFigure('symphonyui.builtin.figures.MeanResponseFigure', obj.rig.getDevice(obj.amp));
                 obj.showFigure('symphonyui.builtin.figures.ResponseStatisticsFigure', obj.rig.getDevice(obj.amp), {@mean, @var}, ...
                     'baselineRegion', [0 obj.preTime], ...
                     'measurementRegion', [obj.preTime obj.preTime+obj.stimTime]);
             else
                 obj.showFigure('edu.washington.riekelab.figures.DualResponseFigure', obj.rig.getDevice(obj.amp), obj.rig.getDevice(obj.amp2));
-                obj.showFigure('edu.washington.riekelab.figures.DualMeanResponseFigure', obj.rig.getDevice(obj.amp), obj.rig.getDevice(obj.amp2), ...
-                    'groupBy1', {'stdv'}, ...
-                    'groupBy2', {'stdv'});
+                obj.showFigure('edu.washington.riekelab.figures.DualMeanResponseFigure', obj.rig.getDevice(obj.amp), obj.rig.getDevice(obj.amp2));
                 obj.showFigure('edu.washington.riekelab.figures.DualResponseStatisticsFigure', obj.rig.getDevice(obj.amp), {@mean, @var}, obj.rig.getDevice(obj.amp2), {@mean, @var}, ...
                     'baselineRegion1', [0 obj.preTime], ...
                     'measurementRegion1', [obj.preTime obj.preTime+obj.stimTime], ...
@@ -101,26 +85,39 @@ classdef LedNoiseFamily < edu.washington.riekelab.protocols.RiekeLabProtocol
                     'measurementRegion2', [obj.preTime obj.preTime+obj.stimTime]);
             end
             
-            device = obj.rig.getDevice(obj.led);
-            device.background = symphonyui.core.Measurement(obj.lightMean, device.background.displayUnits);
+            if strcmp(obj.led1, obj.led2)
+                error('ERROR: LEDs 1 and 2 must be different');
+            end
+
+            device1 = obj.rig.getDevice(obj.led1);
+            device2 = obj.rig.getDevice(obj.led2);
+            device1.background = symphonyui.core.Measurement(obj.mean1, device1.background.displayUnits);
+            device2.background = symphonyui.core.Measurement(obj.mean2, device2.background.displayUnits);
         end
         
-        function [stim, stdv] = createLedStimulus(obj, pulseNum, seed)
-            sdNum = floor((double(pulseNum) - 1) / double(obj.repeatsPerStdv));
-            stdv = obj.stdvMultiplier^sdNum * obj.startStdv;
-            
+        function stim = createLedStimulus(obj, ledNum, seed)
             gen = edu.washington.riekelab.stimuli.GaussianNoiseGeneratorV2();
+
+            if ledNum == 1
+                gen.stDev = obj.stdv1;
+                gen.freqCutoff = obj.frequencyCutoff1;
+                gen.numFilters = obj.numberOfFilters1;
+                gen.mean = obj.mean1;
+                gen.units = obj.rig.getDevice(obj.led1).background.displayUnits;
+            elseif ledNum == 2
+                gen.stDev = obj.stdv2;
+                gen.freqCutoff = obj.frequencyCutoff2;
+                gen.numFilters = obj.numberOfFilters2;
+                gen.mean = obj.mean2;
+                gen.units = obj.rig.getDevice(obj.led2).background.displayUnits;
+            end
             
             gen.preTime = obj.preTime;
             gen.stimTime = obj.stimTime;
             gen.tailTime = obj.tailTime;
-            gen.stDev = stdv;
-            gen.freqCutoff = obj.frequencyCutoff;
-            gen.numFilters = obj.numberOfFilters;
-            gen.mean = obj.lightMean;
+            
             gen.seed = seed;
             gen.sampleRate = obj.sampleRate;
-            gen.units = obj.rig.getDevice(obj.led).background.displayUnits;
             if strcmp(gen.units, symphonyui.core.Measurement.NORMALIZED)
                 gen.upperLimit = 1;
                 gen.lowerLimit = 0;
@@ -128,28 +125,41 @@ classdef LedNoiseFamily < edu.washington.riekelab.protocols.RiekeLabProtocol
                 gen.upperLimit = 10.239;
                 gen.lowerLimit = -10.24;
             end
-            
+
             stim = gen.generate();
         end
         
         function prepareEpoch(obj, epoch)
             prepareEpoch@edu.washington.riekelab.protocols.RiekeLabProtocol(obj, epoch);
             
-            persistent seed;
-            if ~obj.useRandomSeed
-                seed = 0;
-            elseif mod(obj.numEpochsPrepared - 1, obj.repeatsPerStdv) == 0
-                seed = RandStream.shuffleSeed;
+            persistent seed1;
+            persistent seed2;
+            if obj.numEpochsPrepared == 1 % note obj.numEpochsPrepared starts at 1 (before first epoch is prepared)
+                if ~obj.useRandomSeed
+                    seed1 = 0;
+                    seed2 = 1;
+                else
+                    seed1 = RandStream.shuffleSeed;
+                    seed2 = RandStream.shuffleSeed;
+                end
             end
+            epoch.addParameter('seed1', seed1);
+            epoch.addParameter('seed2', seed2);
             
-            pulseNum = mod(obj.numEpochsPrepared - 1, obj.pulsesInFamily) + 1;
-            [stim, stdv] = obj.createLedStimulus(pulseNum, seed);
-            
-            epoch.addParameter('stdv', stdv);
-            epoch.addParameter('seed', seed);
-            epoch.addStimulus(obj.rig.getDevice(obj.led), stim);
+            if mod(obj.numEpochsPrepared - 1, 3) == 0       % LED 1 alone
+                stim = obj.createLedStimulus(1, seed1);
+                epoch.addStimulus(obj.rig.getDevice(obj.led1), stim);
+            elseif mod(obj.numEpochsPrepared - 1, 3) == 1   % LED 2 alone
+                stim = obj.createLedStimulus(2, seed2);
+                epoch.addStimulus(obj.rig.getDevice(obj.led2), stim);
+            else                                            % LEDs 1 and 2 simultaneously
+                stim1 = obj.createLedStimulus(1, seed1);
+                stim2 = obj.createLedStimulus(2, seed2);
+                epoch.addStimulus(obj.rig.getDevice(obj.led1), stim1);
+                epoch.addStimulus(obj.rig.getDevice(obj.led2), stim2);
+            end
+
             epoch.addResponse(obj.rig.getDevice(obj.amp));
-            
             if numel(obj.rig.getDeviceNames('Amp')) >= 2
                 epoch.addResponse(obj.rig.getDevice(obj.amp2));
             end
@@ -158,16 +168,18 @@ classdef LedNoiseFamily < edu.washington.riekelab.protocols.RiekeLabProtocol
         function prepareInterval(obj, interval)
             prepareInterval@edu.washington.riekelab.protocols.RiekeLabProtocol(obj, interval);
             
-            device = obj.rig.getDevice(obj.led);
-            interval.addDirectCurrentStimulus(device, device.background, obj.interpulseInterval, obj.sampleRate);
+            device1 = obj.rig.getDevice(obj.led1);
+            interval.addDirectCurrentStimulus(device1, device1.background, obj.interpulseInterval, obj.sampleRate);
+            device2 = obj.rig.getDevice(obj.led2);
+            interval.addDirectCurrentStimulus(device2, device2.background, obj.interpulseInterval, obj.sampleRate);
         end
-        
+
         function tf = shouldContinuePreparingEpochs(obj)
-            tf = obj.numEpochsPrepared < obj.numberOfAverages * obj.pulsesInFamily;
+            tf = obj.numEpochsPrepared < 3 * obj.numberOfAverages;
         end
         
         function tf = shouldContinueRun(obj)
-            tf = obj.numEpochsCompleted < obj.numberOfAverages * obj.pulsesInFamily;
+            tf = obj.numEpochsCompleted < 3 * obj.numberOfAverages;
         end
         
         function a = get.amp2(obj)
